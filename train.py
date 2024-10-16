0c27c6eb6cac8b1307ca9871b45515a4ed716abd
Mohammad Moghimi <135135282+Mohammad-Moghimi@users.noreply.github.com>
2024-10-16 03:50:04 +0330

    Create train.py

---
+++ train.py
@@ -0,0 +1,19 @@
+def get_encoder(config):
+    """
+    Initialize the ManyHotEncoder for the UrbanSound8K dataset.
+
+    Args:
+        config: dict, configuration parameters including audio length, frame length, etc.
+
+    Returns:
+        ManyHotEncoder instance
+    """
+    encoder = ManyHotEncoder(
+        labels=list(classes_labels_urbansound8k.keys()),
+        audio_len=config["data"]["audio_max_len"],
+        frame_len=config["feats"]["frame_len"],
+        frame_hop=config["feats"]["frame_hop"],
+        net_pooling=config["data"].get("net_pooling", 1),
+        fs=config["data"]["fs"],
+    )
+    return encoder
