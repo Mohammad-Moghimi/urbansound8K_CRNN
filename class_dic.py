aedb48789a3ce3f76306bfe2789edb030df1e85d
Mohammad Moghimi <135135282+Mohammad-Moghimi@users.noreply.github.com>
2024-10-16 03:33:40 +0330

    Create class_dic.py

---
+++ class_dic.py
@@ -0,0 +1,17 @@
+from collections import OrderedDict
+
+# Dictionary mapping class names to numerical labels for UrbanSound8K
+classes_labels_urbansound8k = OrderedDict(
+    {
+        "air_conditioner": 0,
+        "car_horn": 1,
+        "children_playing": 2,
+        "dog_bark": 3,
+        "drilling": 4,
+        "engine_idling": 5,
+        "gun_shot": 6,
+        "jackhammer": 7,
+        "siren": 8,
+        "street_music": 9,
+    }
+)
